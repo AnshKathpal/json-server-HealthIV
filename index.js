d4e01ee7f69abd3225d483ed3f7e87f6e2fcf10d
AnshKathpal <storage.2.3107@gmail.com>
2023-05-11 18:45:04 +0530

    check

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router(`db.json`)
 const middlewares = jsonServer.defaults();
+
 server.use(middlewares);
 server.use(router);
 server.listen(process.env.PORT || 3000,() => {
